df6e48bb1d8f9b640ce539a56321166ff8f1d3f3
marwanmattar21 <marwanpubgmr11@gmail.com>
2025-11-17 20:22:17 +0200

    https://github.com/marwanmattar21/audio-player.git

---
+++ cpp.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    
+    };
+    class linkedlist{
+    
+     public:
+        Node* head;
+        linkedlist(){
+           head=NULL;
+        }
+        bool isempty(){
+    if(head==NULL)
+    return true;
+    else
+    return false;
+        }
+    
+        void insertfirst(int newitem){
+            Node* newnode=new Node();
+            newnode->data=newitem;
+            if (isempty()){
+            newnode->next=NULL;
+            head=newnode;
+        }
+        else{
+            newnode->next=head;
+            head=newnode;
+        }
+    
+    
+        }
+        
+        void display(){
+            Node* temp=head;
+            while(temp!=NULL){
+                cout<<temp->data<<" ";
+                temp=temp->next;       
+             }
+      
+    
+        }
+    
+    };
+    
+    int main(){
+        linkedlist lis;
+        lis.insertfirst(10);
+        lis.display();
+    
+    cout<<"maro";
+    
+    }
---
+++ m.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std:: cout << "maro";
+    return 0;
+}
